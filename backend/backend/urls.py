1ed39942d21833c5b66f259683265583f5f32ff3
N1ph1l1m <zerorad@inbox.ru>
2024-09-27 16:55:16 +0300

    v.0.8.5.3 update model User and data in db

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from .yasg import urlpatterns as doc_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,3 +31,4 @@ urlpatterns = [
 ]
 
 urlpatterns += doc_urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username"]
+        fields = ["id", "username","photo"]
         extra_kwargs = {'password': {'write_only': True}}
 
         def create(self, validated_data):
